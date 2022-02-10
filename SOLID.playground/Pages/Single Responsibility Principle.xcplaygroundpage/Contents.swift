78c04272c506180498da1c4e6f2005935ed0df1d
Ahmet Mucahid Bozkurt <mail@ahmetmb.com>
2022-02-10 21:08:48 +0300

    - Added single responsibility principle

--- SOLID.playground/Pages/Single Responsibility Principle.xcplaygroundpage/Contents.swift
+++ SOLID.playground/Pages/Single Responsibility Principle.xcplaygroundpage/Contents.swift
@@ -2,6 +2,38 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// Tasarladigimiz modul, class, fonksiyon vs. tek bir ise odaklanmalidir.
+// Degisim tek bir sebepten kaynaklanmalidir.
 
-//: [Next](@next)
+// YANLIS
+struct Developer {
+    let name: String
+    let salary: Decimal
+    private let taxPercentage: Decimal
+    
+    func calculateTax() -> Decimal {
+        return salary * taxPercentage
+    }
+}
+
+// Genisletilebilir degil yani burada yeni meslek eklemek istesek, bir sekilde duplicate edecegiz ya da vergi orani degisirse. tacPercentage, Developer struct altinda olmasi mantikli degil, cunku ortak noktalari yok.
+
+// DOGRU
+struct Engineer {
+    let name: String
+    let salary: Decimal
+}
+
+// Calculate fonksiyonunu ayri bir class'a alarak isleme baslayalim. Artik tek bir islem yapiyor. Yeni bir meslek grubu ekledigimiz zaman ayni fonksiyonu kullanabilecegiz.
+
+class TaxCalculator {
+    private let taxPercentage: Decimal
+    
+    init(taxPercentage: Decimal) {
+        self.taxPercentage = taxPercentage
+    }
+    
+    func calculateTax(forSalary salary: Decimal) -> Decimal {
+        return salary * taxPercentage
+    }
+}
