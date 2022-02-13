3f3c0863749c8405be7c718591a8d526a2a2a4b6
Ahmet Mucahid Bozkurt <mail@ahmetmb.com>
2022-02-13 17:16:49 +0300

    - Added dependency inversion principle

--- SOLID.playground/Pages/Dependency Inversion Principle.xcplaygroundpage/Contents.swift
+++ SOLID.playground/Pages/Dependency Inversion Principle.xcplaygroundpage/Contents.swift
@@ -1,7 +1,52 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+// Sınıflar arası bağımlılıkların en aza indirgenmesi gerektiğini söyleyen prensiptir. Özellikle alt sınıflarda olan bir değişikliğin üst sınıfları etkilememesi gerektiğini dile getirir.
+// Objeleri direkt birbiriyle referans etmektense, bir interface arkasina saklamak gerekir. Yani viewController icerisindeki viewModel degiskenine CartViewModel'i depend etmek yanlis.
 
-var greeting = "Hello, playground"
+// YANLIS
+class CarViewModel {
+    let make: String = "Porsche"
+    let model: String = "911"
+}
 
-//: [Next](@next)
+class CarViewController: UIViewController {
+    var viewModel: CarViewModel!
+    
+    @IBOutlet private var makeLabel: UILabel!
+    @IBOutlet private var modelLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeLabel.text = viewModel.make
+        makeLabel.text = viewModel.model
+    }
+}
+
+// DOGRU
+protocol CarViewModelProtocol {
+    var make: String { get }
+    var model: String { get }
+}
+
+class PorscheViewModel: CarViewModelProtocol {
+    let make: String = "Porsche"
+    let model: String = "911"
+}
+
+class OpelViewModel: CarViewModelProtocol {
+    var make: String = "Opel"
+    var model: String = "Astra"
+}
+
+class CarViewController2: UIViewController {
+    var viewModel: CarViewModelProtocol!
+    
+    @IBOutlet private var makeLabel: UILabel!
+    @IBOutlet private var modelLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeLabel.text = viewModel.make
+        makeLabel.text = viewModel.model
+    }
+}
